15de1b46ce154c33925978e5c38fad6051f39bfc
adamtyrrell <67089521+adamtyrrell@users.noreply.github.com>
2021-11-03 21:17:37 -0500

    css cleaned up for header and content area

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -4,8 +4,13 @@ import HeaderLink from './HeaderLinksComponent';
 
 function Header() {
     return (
-        <header>
-            <div className="bg-gray-100 min-w-full flex justify-evenly pr-10 pt-1 pb-1 font-semibold text-sm text-center sm:text-right"><p>Sales: 888.000.000 </p> <p>Support</p> <p>Login</p></div>
+        <header className="sticky top-0 bg-white">
+            <div className="bg-gray-100 min-w-full flex justify-evenly pr-10 pt-1 pb-1 
+            font-semibold text-md tracking-wider text-center sm:text-right">
+                <p className="hover:text-red-500 cursor-pointer">Sales: 888.000.000 </p> 
+                <p className="hover:text-red-500 cursor-pointer">Support</p> 
+                <p className="hover:text-red-500 cursor-pointer">Login</p>
+            </div>
             <div className="flex flex-col sm:flex-row m-5 justify-evenly">
                 <div className="flex flex-grow justify-evenly"><img src={logo} alt="logo" width="300"className="max-w-sm mb-2 object-contain"/></div>
                 <div className="flex flex-grow justify-evenly max-w-2xl h-8 mt-2">
--- src/components/SecondContentBox.js
+++ src/components/SecondContentBox.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import './SecondContentBox.css'
+import uploadImg from '../images/upload.png';
 
 function SecondContent() {
     return (
-        <div className="content-box text-gray-100 flex flex-col">
-            This is the second content box
+        <div>
+            <div className="content-box"></div>
+            <div className="overlay">
+            <h3 className="ml-10 font-bold text-lg tracking-wider md:text-2xl md:ml-20">Data Extraction</h3>
+                <div className="">
+                    
+                    <hr className=""/>
+                    
+                </div>
+                <div>
+                    <img src={uploadImg} alt="upload" className="p-5 circle-img"/>
+                    <p className="ml-10 md:ml-20">Seemlessly pull data from your current CMS. Our system will automatically interpret the data into powerful reports to help tailor your business.</p>
+                </div>
+            </div>
         </div>
     )
 }
